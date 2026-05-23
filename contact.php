4fec100215e5ef037d68fc18910ec67582fe1734
lowcw <lowcwedgetech@gmail.com>
2026-05-23 20:05:30 +0800

    left4page

--- contact.php
+++ contact.php
@@ -16,47 +16,53 @@
 <body>
     <?php include('header.php'); ?>
     
-    <section class="banner-c d-flex align-items-center">
-        <div class="container d-flex justify-content-center">
-          <div class="row">
-            <div class="col-12">
+    <section class="banner-c pb-5">
+        <div class="custom-width container my-5 my-md-5">
+          <div class="row mx-3 mx-md-0">
+            <div class="col-12 my-3 my-md-0">
               <h5>CONTACT US</h5>
             </div>
             <div class="col-12">
               <h1>Hej! Let's rock together</h1>
             </div>
+            <div class="col-12 d-flex justify-content-center">
+              <div class="banner-p-w my-5">
+                <p class="banner-p">Agile frameworks to provide a robust high level overviews. Iterative approaches to corporate 
+                strategy foster collaborative thinking to further the all new overall proposition.</p>
+              </div>
+            </div>
           </div>
         </div>
     </section>
-    <section class="p-0">
+    <section class="p-0 ">
       <div>
         <img src="assets/img/contact.jpg" alt="" class="img-fluid custom-vh">
       </div>
-      <div class="d-flex align-items-center justify-content-center">
-    <div class="m-person-d container" style="padding:50px;">
+      <div class="d-flex align-items-center justify-content-center mx-3 mx-md-0">
+    <div class="m-person-d container">
       <div class="row">
-        <div class="col-4">
+        <div class="col-12 col-md-4 pb-4 pb-md-0">
           <div class="row row-col-auto">
             <h3> New York </h3>
           </div>
           <div class="row row-col-auto">
-            <p>438 West Virginia Avenue New York, United States +33 1 488-170-8219</p>
+            <p>438 West Virginia Avenue <br>New York, United States <br>+33 1 488-170-8219</p>
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-md-4 pb-4 pb-md-0">
           <div class="row row-col-auto">
             <h3> New York </h3>
           </div>
           <div class="row row-col-auto">
-            <p>438 West Virginia Avenue New York, United States +33 1 488-170-8219</p>
+            <p>438 West Virginia Avenue <br>New York, United States <br>+33 1 488-170-8219</p>
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-md-4">
           <div class="row row-col-auto">
             <h3> New York </h3>
           </div>
           <div class="row row-col-auto">
-            <p>438 West Virginia Avenue New York, United States +33 1 488-170-8219</p>
+            <p>438 West Virginia Avenue <br>New York, United States <br>+33 1 488-170-8219</p>
           </div>
         </div>
       </div>
@@ -64,48 +70,65 @@
   </div>
 </section>
 <section>
-  <div class="container d-flex justify-content-center">
-    <div class="row">
-      <div class="col-6">
+  <div class="w-contact container pt-md-5">
+    <div class="row justify-content-center align-items-center mx-3 mx-md-0">
+      <div class="col-12 col-md-6">
+        <div class="d-flex flex-column justify-content-between gap-0 gap-md-5">
         <div class="row row-col-auto">
+          <div class="form-group">
           <h4>Get in contact</h4>
         </div>
         <div class="row row-col-auto">
-          <p class="contact">It's time to create something great and remarkable together...</p>
+          <p class="contact-o">It's time to create something great and remarkable together...</p>
+        </div>
         </div>
         <div class="row row-col-auto my-5">
+          <div class="bottom-end">
           <div class="d-flex align-items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="contact-svg bi bi-envelope" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-white contact-svg bi bi-envelope" viewBox="0 0 16 16">
               <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
             </svg>
             <p class="contact mb-0">info@yourwebsite.com</p>
           </div>
         </div>
-        <div class="row row-col-auto my-5">
+        <div class="row row-col-auto mt-4 my-md-5">
           <div class="d-flex align-itmes-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="contact-svg bi bi-telephone" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-white contact-svg bi bi-telephone" viewBox="0 0 16 16">
               <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
             </svg>
             <p class="contact mb-0">914-934-3631</p>
           </div>
+          </div>
+          </div>
         </div> 
       </div>
-      <div class="col-6">
-        <div class="row row-col-auto">
-          <h4>Get in contact</h4>
+      <div class="col-12 col-md-6">
+        <div class="d-flex flex-column justify-content-between gap-5">
+          <div class="top-start">
+          <div class="row row-col-auto">
+            <form>
+              <div class="form-group">
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Name">
+              </div>
+            </form>
+          </div>
+          </div>
+          <div class="bottom-end">
+            <div class="row row-col-auto">
+              <form>
+                <div class="form-group">
+                  <textarea class="form-control" id="exampleFormControlTextarea1"placeholder="Message" rows="10"></textarea>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="">
+          <button class="btn  px-5 py-3" type="submit">Submit a request</button>
         </div>
-        <div class="row row-col-auto">
-          <p class="contact">It's time to create something great and remarkable together...</p>
         </div>
-        <div class="row row-col-auto">
-          <div class="d-flex align-items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-            </svg>
-            <p class="contact mb-0">info@yourwebsite.com</p>
-          </div>  
-        </div>
+        
       </div>
+      
     </div>
   </div>
 
