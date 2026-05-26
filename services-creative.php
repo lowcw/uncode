40044f2829353515da0baa61673e9ff45ef5a638
lowcw <lowcwedgetech@gmail.com>
2026-05-26 10:01:46 +0800

    last_3

---
+++ services-creative.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- My CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <link rel="stylesheet" href="assets/css/service.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+    
+    <section class="banner-c pb-5">
+        <div class="custom-width container my-5 my-md-5">
+          <div class="row mx-3 mx-md-0">
+            <div class="col-12 my-3 my-md-0">
+              <h5 class="text-uppercase">What we do</h5>
+            </div>
+            <div class="col-12">
+              <h1>Suuuper duper web stuff</h1>
+            </div>
+            <div class="col-12 d-flex justify-content-center">
+              <div class="banner-p-w my-5">
+                <p class="banner-p">Iterative approaches to corporate strategy foster collaborative thinking to 
+                    further the overall value. Leverage agile frameworks to provide a robust high level overviews.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+<section class="p-0 m-0">
+    <div class="bannner-2">
+        <img src="assets/img/service-bg.jpg" alt="" class="img-fluid custom-vh">
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="service-box container">
+          <div class="row ">
+           <div class="col-12 col-md-4 service-1">   
+             <div class="row">
+                <div class="col-12 p-0 mb-4 mb-md-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-phone-icon text-white bi bi-phone" viewBox="0 0 16 16">
+                    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                    </svg>
+                </div>
+                <div class="col-12">
+                    <h3> Web-Design and Prototype </h3>
+                </div>
+                <div class="col-12">
+                    <p>Iterative approaches corporate strategy foster collaborative thinking</p>
+                </div>
+             </div>
+            </div>
+            <div class="col-12 col-md-4 service-2">   
+             <div class="row">
+                <div class="col-12 p-0 mb-4 mb-md-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-phone-icon text-white bi bi-phone" viewBox="0 0 16 16">
+                    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                    </svg>
+                </div>
+                <div class="col-12">
+                    <h3> Wireframes and Design </h3>
+                </div>
+                <div class="col-12">
+                    <p>Organically grow the holistic world view of disruptive innovation.</p>
+                </div>
+             </div>
+            </div>
+            <div class="col-12 col-md-4 service-3">   
+             <div class="row">
+                <div class="col-12 p-0 mb-4 mb-md-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-phone-icon text-white bi bi-phone" viewBox="0 0 16 16">
+                    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                    </svg>
+                </div>
+                <div class="col-12">
+                    <h3> Testing and Evaluation </h3>
+                </div>
+                <div class="col-12">
+                    <p>Collaborative thinking further the overall value settled proposition.</p>
+                </div>
+             </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="bg-service" style="background-color: #141618;">
+</section>
+
+<section class="bg-1-sevice">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 service-p align-content-center">
+                <div class="row">
+                <div class="col-12">
+                    <h2 class="">UX & Prototyping</h2>
+                </div>
+                <div class="col-12">
+                    <p class="p-color">Approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic 
+                    world view of disruptive innovation via workplace diversity generated content in real-time.</p>
+                </div>
+                <div class="row my-3">
+                <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Architecture</h6>
+              </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>User Interface Design</h6>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 mt-4 mt-md-0">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Research & Discovery </h6>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Rapid Prototyping</h6>
+                </div>
+              </div>
+            </div>
+             </div>
+            </div>
+
+      <div class="col-12 col-md-6 order-first order-md-0">
+        <img src="assets/img/service-content.jpg" alt="" 
+        class="img-fluid service-img">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section >
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <img src="assets/img/service-content.jpg" alt="" 
+                class="img-fluid service-img">
+            </div>
+
+            <div class="col-12 col-md-6 service-p align-content-center">
+                <div class="row">
+                <div class="col-12">
+                    <h2 class="">UX & Prototyping</h2>
+                </div>
+                <div class="col-12">
+                    <p class="p-color">Approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic 
+                    world view of disruptive innovation via workplace diversity generated content in real-time.</p>
+                </div>
+                <div class="row my-3">
+                <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Architecture</h6>
+              </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>User Interface Design</h6>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 mt-4 mt-md-0">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Research & Discovery </h6>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Rapid Prototyping</h6>
+                </div>
+              </div>
+            </div>
+             </div>
+            </div>
+    </div>
+  </div>
+</section>
+
+<section class="bg-1-sevice">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 service-p align-content-center">
+                <div class="row">
+                <div class="col-12">
+                    <h2 class="">UX & Prototyping</h2>
+                </div>
+                <div class="col-12">
+                    <p class="p-color">Approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic 
+                    world view of disruptive innovation via workplace diversity generated content in real-time.</p>
+                </div>
+                <div class="row my-3">
+                <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Architecture</h6>
+              </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>User Interface Design</h6>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 mt-4 mt-md-0">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Research & Discovery </h6>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="service-check-icon bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+                <h6>Rapid Prototyping</h6>
+                </div>
+              </div>
+            </div>
+             </div>
+            </div>
+
+      <div class="col-12 col-md-6 order-first order-md-0">
+        <img src="assets/img/service-content.jpg" alt="" 
+        class="img-fluid service-img">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section style="background-color: #141618;">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12 text-white">
+                <h2>Global partners</h2>
+            </div>
+            <div class="col-12 px-5 d-flex justify-content-center">
+                <p class="p-color-Global w-0 w-md-75">Iterative approaches to corporate strategy foster collaborative thinking 
+                    to further the overall value proposition organically grow the holistic world view of disruptive innovation.</p>
+            </div>
+        </div>
+        <div class="row mt-2 mt-md-5 text-center">
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+        </div>
+        <div class="row mt-2 mt-md-5 text-center">
+           <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+            <div class="col-4 col-md-2">
+                <img src="assets/img/logo-americanservice.png" class="img-fluid logo-service">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="l-p">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12">    
+                <h5 class="text-uppercase" style="color:#FFFFFF;">Start a project</h5>
+            </div>
+            <div class="col-12">
+                <h2 style="color:#FFFFFF;">Ready to rock together?!</h2>
+            </div>
+            <div class="col-12 mt-4">
+                <button class="btn-lp px-4 py-3"  onclick="window.location.href='../index.php'">Contact Us</button>
+            </div>
+        </div>
+    </div>
+</section>
+    <?php include('footer/footer.php'); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
